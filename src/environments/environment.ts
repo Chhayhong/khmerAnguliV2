27ec0fc14f218f7719d435d320362b0829a9692e
thatchhayhong <thatchhayhong@gmail.com>
2022-10-25 22:41:55 +0700

    new layout

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'khmeranguliv2',
+    appId: '1:548250266113:web:74979f11384b713c7b6e00',
+    databaseURL: 'https://khmeranguliv2-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'khmeranguliv2.appspot.com',
+    apiKey: 'AIzaSyBV2dLHTEbiLhaNJXlmo7inDXB-Wsfg0Lk',
+    authDomain: 'khmeranguliv2.firebaseapp.com',
+    messagingSenderId: '548250266113',
+  },
   production: false
 };
 
